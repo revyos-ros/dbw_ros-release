ab6df056d8b760ed573b57867d29b7399d92e2b0
Kevin Hallenbeck <khallenbeck@dataspeedinc.com>
2022-12-01 17:24:24 -0500

    Rebase from 'upstream'

--- dataspeed_ulc_can/include/dataspeed_ulc_can/dispatch.hpp
+++ dataspeed_ulc_can/include/dataspeed_ulc_can/dispatch.hpp
@@ -40,7 +40,8 @@ namespace dataspeed_ulc_can {
 #pragma pack(push, 1)  // Pack structures to a single byte
 
 typedef struct {
-  int16_t linear_velocity  :16; // 0.0025 m/s, -81.920 to 81.915 m/s, +-183 mph
+  int16_t lon_command      :16; // speed mode: 0.0025 m/s, -81.920 to 81.915 m/s, +-183 mph
+                                // accel mode: 5e-4 m/s^2, -16.384 to 16.3835 m/s^2
   int16_t yaw_command      :16; // yaw rate mode: 0.00025 rad/s, -8.1920 to 8.1915 rad/s
                                 // curvature mode: 0.0000061 1/m, -0.1999 1/m to 0.1999 1/m
 
@@ -50,7 +51,8 @@ typedef struct {
   uint8_t enable_steering  :1;
   uint8_t enable_pedals    :1;
   uint8_t clear            :1;
-  uint8_t                  :2;
+  uint8_t pedals_mode      :1;  // 0 = speed mode, 1 = accel mode
+  uint8_t coast_decel      :1;
 
   uint8_t                  :8;
   uint8_t                  :8;
@@ -59,12 +61,12 @@ typedef struct {
 static_assert(8 == sizeof(MsgUlcCmd));
 
 typedef struct {
-    uint8_t linear_accel;   // 0.025 m/s^2, 0 to 6.375 m/s^2
-    uint8_t linear_decel;   // 0.025 m/s^2, 0 to 6.375 m/s^2
-    uint8_t lateral_accel;  // 0.05 m/s^2, 0 to 12.75 m/s^2
-    uint8_t angular_accel;  // 0.02 rad/s^2, 0 to 5.1 rad/s^2
-    uint8_t :8;
-    uint8_t :8;
+    uint8_t linear_accel;           // 0.025 m/s^2, 0 to 6.375 m/s^2
+    uint8_t linear_decel;           // 0.025 m/s^2, 0 to 6.375 m/s^2
+    uint8_t lateral_accel;          // 0.05 m/s^2, 0 to 12.75 m/s^2
+    uint8_t angular_accel;          // 0.02 rad/s^2, 0 to 5.1 rad/s^2
+    uint8_t jerk_limit_throttle;    // 0.1 m/s^3, 0 to 25.5 m/s^3
+    uint8_t jerk_limit_brake;       // 0.1 m/s^3, 0 to 25.5 m/s^3
     uint8_t :8;
     uint8_t wdc;
 } MsgUlcCfg;
@@ -74,7 +76,7 @@ typedef struct {
   int16_t speed_ref :13; // 0.02 m/s,
   uint16_t timeout :1;
   uint16_t pedals_enabled :1;
-  uint16_t tracking_mode :1;
+  uint16_t pedals_mode :1;
   int16_t speed_meas :13; // 0.02 m/s
   uint16_t override :1;
   uint16_t steering_enabled :1;
@@ -82,12 +84,21 @@ typedef struct {
   int8_t  accel_ref; // 0.05 m/s^2
   int8_t accel_meas; // 0.05 m/s^2
   uint8_t max_steering_angle: 7; // 5 deg
-  uint8_t:1;
+  uint8_t coasting: 1;
   uint8_t max_steering_vel :6; //  8 deg/s
   uint8_t steering_preempted: 1;
   uint8_t speed_preempted: 1;
 } MsgUlcReport;
 static_assert(8 == sizeof(MsgUlcReport));
+
+typedef struct {
+  uint8_t module;
+  uint8_t platform;
+  uint16_t major;
+  uint16_t minor;
+  uint16_t build;
+} MsgVersion;
+static_assert(8 == sizeof(MsgVersion));
 
 #pragma pack(pop)  // Undo packing
 
@@ -95,6 +106,7 @@ enum {
   ID_ULC_CMD            = 0x076,
   ID_ULC_CONFIG         = 0x077,
   ID_ULC_REPORT         = 0x078,
+  ID_VERSION            = 0x07F,
 };
 
 } // namespace dataspeed_ulc_can
